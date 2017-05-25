5d26b0e65a8f5c3454ae7231867861862208753e
Régis Gaidot <rgaidot@gmail.com>
2017-05-25 02:38:00 +0200

    bump npm modules

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export default {
     api: {
         url: 'https://api.themoviedb.org',
         url_images: 'https://image.tmdb.org/t/p',
-        key: 'YOUR-KEY',
+        key: '***REMOVED***',
         lang: 'fr-FR',
         initSortBy: 'popularity.desc',
         initYear: '2017'
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 
-export default function configureStore(rootReducer, initialState) {
+export default function configureStore(
+    rootReducer: ?() => void,
+    initialState: ?() => void
+) {
     const loggerMiddleware = createLogger();
     const middlewares = applyMiddleware(loggerMiddleware, thunkMiddleware);
 
